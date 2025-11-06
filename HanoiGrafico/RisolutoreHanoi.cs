d554033cf81f9dbae309dbf9f62c37b738e0ffb6
12999427 <12999427@INFO1-23>
2025-11-06 11:21:53 +0100

    fattooo

--- HanoiGrafico/RisolutoreHanoi.cs
+++ HanoiGrafico/RisolutoreHanoi.cs
@@ -22,10 +22,24 @@ namespace HanoiGrafico
             {
                 int numMinimoMosse = 0;
                 numMinimoMosse += Hanoi(numDischi - 1, DA, ALTRO, A, cronologia);
-                numMinimoMosse += Hanoi(1, DA, A, ALTRO, cronologia);
+                numMinimoMosse += Hanoi(1, DA, A, ALTRO, cronologia); // oppure // cronologia.Add((DA, A)); numMinimoMosse++;
                 numMinimoMosse += Hanoi(numDischi - 1, ALTRO, A, DA, cronologia);
                 return numMinimoMosse;
             }
+            /*if (numDischi == 1)
+            {
+                cronologia.Add((DA, A));
+                return 1;
+            }
+            else
+            {
+                int numMinimoMosse = 0;
+                numMinimoMosse += Hanoi(numDischi - 1, DA, ALTRO, A, cronologia);
+                //numMinimoMosse += Hanoi(1, DA, A, ALTRO, cronologia);
+                cronologia.Add((DA, A));
+                numMinimoMosse += Hanoi(numDischi - 1, ALTRO, A, DA, cronologia);
+                return numMinimoMosse;
+            }*/
         }
     }
 }
